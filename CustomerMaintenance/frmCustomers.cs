02ef1a9020ca02f96a764eaa11212c80b2d7ba2f
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-06-18 12:30:39 -0700

    code and comments

--- CustomerMaintenance/frmCustomers.cs
+++ CustomerMaintenance/frmCustomers.cs
@@ -15,14 +15,14 @@ using System.Windows.Forms;
 * Dominique Tepper, 18JUN2022
 * 
 * 8.  Declare customers class var, list tyoe, null
-* 9a. Load event handler that uses GetCustomers() of the
-*     CustomerDB class, adds customers to listbox.
-* 9b. Use GDT() to format custoemr data
+* 9.  Load event handler that uses GetCustomers() of the
+*     CustomerDB class, adds customers to listbox. Use 
+*     GDT() to format custoemr data
 * 10. Add btnAdd_Click event handler that creates a new
 *     instance of GNC().
-*     If data != null => add new customer obj, call
+*     If data != null => (a) add new customer obj, (b) call
 *     SaveCustomers() of CustomersDB class to save list,
-*     refresh Customer listbox.
+*     (c) refresh Customer listbox.
 * 11. btnDelete_Click event handler
 *     a. removes selected custoemr
 *     b. calls SaveCustomers()
@@ -37,6 +37,72 @@ namespace CustomerMaintenance
         public frmCustomers()
         {
             InitializeComponent();
+        }
+
+        //8. var customer, list, null
+        private List<Customer> customers = null;
+
+        //9. Load event handler
+        //Tepper, 18JUN2022
+        private void frmCustomers_Load(object sender, EventArgs e)
+        {
+            customers = CustomerDB.GetCustomers();
+            FillCustomerListBox(); 
+        }
+
+        //9. fills list box with customers from DB
+        //uses GDT() to format data
+        //Tepper, 18JUN2022
+        private void FillCustomerListBox()
+        {
+            lstCustomers.Items.Clear();
+            foreach (Customer c in customers)
+            {
+                lstCustomers.Items.Add(c.GetDisplayText());
+            }
+        }
+
+        //10. btnAdd event handler that instantiates
+        // GetNewCustomer()
+        // Tepper, 18JUN2022
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmAddCustomer addCustomerForm = new frmAddCustomer();
+            Customer customer = addCustomerForm.GetNewCustomer();
+            if (customer != null)
+            {
+                customers.Add(customer); //10a
+                CustomerDB.SaveCustomers(customers); //10b
+                FillCustomerListBox(); //10c
+            }
+        }
+
+        //11. btnDelete event handler
+        //Tepper, 18JUN2022
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int i = lstCustomers.SelectedIndex;
+            if (i != -1)
+            {
+                Customer customer = customers[i];
+                string message = "Are you sure you want to delete "
+                    + customer.FirstName + " " + customer.LastName + "?";
+                DialogResult button = MessageBox.Show(message, "Confirm Delete",
+                    MessageBoxButtons.YesNo);
+                if (button == DialogResult.Yes)
+                {
+                    customers.Remove(customer); //1a
+                    CustomerDB.SaveCustomers(customers); //11b
+                    FillCustomerListBox();   //11c
+                }
+            }
+        }
+
+        //btnExit event handler
+        //Tepper, 18JUN2022
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
 
     }
